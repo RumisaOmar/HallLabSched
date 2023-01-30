7af82c4e1b8a898ed3766d6ffc061972039b6587
logy <logainmohamed22@gmail.com>
2023-01-30 12:49:09 +0200

    App Version 1

---
+++ ShedualeGenerator/ShedualeGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShedualeGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
